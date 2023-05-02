8a71283994b043df91308594e7ab475551f6bd0f
benjamin <benjabarrionuevo2001@gmail.com>
2023-05-02 16:20:37 -0300

    Primer Commit

---
+++ base de datos.sql
@@ -0,0 +1,22 @@
+use avitmat;
+
+insert into stock values(1, 'varios', 'disco corte metal 115mm', 100);
+insert into stock values(2, 'electricidad', 'cable 6mm2 celeste', 50);
+insert into stock values(3, 'pintura', 'pincel N°15', 150);
+insert into stock values(4, 'maquinas', 'amoladora 115mm', 10);
+	
+
+select * from stock;
+
+
+
+
+
+
+
+
+
+
+
+
+
